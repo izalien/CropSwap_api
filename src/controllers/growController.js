7bab96584baa73111a11335c70d998701484048b
izalien <izaliendecker@gmail.com>
2025-03-05 19:55:09 -0500

    in progress remove field functionality

--- src/controllers/growController.js
+++ src/controllers/growController.js
@@ -3,12 +3,34 @@ const router = express.Router();
 const Field = require('../models/fieldEntity');
 const Grow = require('../models/growEntity');
 
-router.post('/grows/create', async (req, res) => {
+router.post('/grows/createNew', async (req, res) => {
     try {
         const newField = await Field.insertOne(req.body.field);
         const newGrow = await Grow.insertOne({
             season: req.body.season,
             field: newField,
+            crop: req.body.crop
+        });
+        res.status(200).json({
+            status: 'success',
+            results: newGrow.length,
+            data: {
+                newGrow
+            }
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: 'fail',
+            message: err
+        });
+    }
+});
+
+router.post('/grows/createFromExisting', async (req, res) => {
+    try {
+        const newGrow = await Grow.insertOne({
+            season: req.body.season,
+            field: req.body.field,
             crop: req.body.crop
         });
         res.status(200).json({
